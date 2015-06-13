74b014d78eb2c909c3df757830c76c49bda0fed0
Daniel Fone <daniel@fone.net.nz>
2015-06-13 23:56:07 +1200

    Remove memory profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  after_action do
-    memory_usage = `ps -o rss= -p #{Process.pid}`.to_i
-    puts "Ruby process memory usage #{memory_usage} KB"
-  end
 end
