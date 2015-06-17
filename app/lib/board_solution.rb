0b9edd6966ee2abba93f251e5ab8aa2c51ca30c4
Daniel Fone <daniel@fone.net.nz>
2015-06-18 00:25:27 +1200

    Keep answers cache clean

--- app/lib/board_solution.rb
+++ app/lib/board_solution.rb
@@ -13,7 +13,7 @@ private
   def check_answer
     @check_answer ||= begin
       Board.increment_counter :guesses, board_id
-      cache.fetch ["answers", board_id] do
+      cache.fetch answer_cache_key do
         # Don't load potentially huge `population` field
         Board.select(:answer).find(board_id).answer
       end
@@ -21,11 +21,16 @@ private
   end
 
   def mark_solved
+    cache.delete answer_cache_key
     Board.where(id: board_id).update_all solved: true
   end
 
   def cache
     Rails.cache
+  end
+
+  def answer_cache_key
+    ["answers", board_id]
   end
 
 end
