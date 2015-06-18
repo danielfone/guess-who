f5d0755633fdffd78c9a6dfb01c3ff74ffca791e
Daniel Fone <daniel@fone.net.nz>
2015-06-18 17:04:53 +1200

    Super tight SQL scoring

--- app/lib/scoreboard.rb
+++ app/lib/scoreboard.rb
@@ -1,27 +1,22 @@
 class Scoreboard
 
-  def board_count
-    Board.count
-  end
-
-  def rounds
-    @rounds ||= Board.distinct.order('round DESC').pluck(:round)
-  end
-
   def rounds_with_scores
-    @rounds_with_scores ||= rounds.each_with_object({}) do |round, h|
-      h[round] = scores_for_round(round)
-    end
+    # :-O
+    @rounds_with_scores ||= scores.each_with_object(Hash.new) {|((r,t),s),h| (h[r] ||= {})[t] = s }.sort.reverse
   end
 
   def velocity
-    @velocity ||= Board.where(solved: true).where('created_at > ?', 1.minute.ago).group('team').sum('size / guesses')
+    @velocity ||= Board.where(solved: true).where('created_at > ?', 5.seconds.ago).group('team').sum('size / guesses * 12')
   end
 
 private
 
   def scores_for_round(round)
     Board.where(solved: true, round: round).group('team').order('sum_size_guesses DESC').sum('size / guesses')
+  end
+
+  def scores
+    @scores ||= Board.where(solved: true).group('round', 'team').order('sum_size_guesses DESC').sum('size / guesses')
   end
 
 end
